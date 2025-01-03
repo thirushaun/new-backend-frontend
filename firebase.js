86507bdecfa0d1b2ee5a47dd47e59c6f58e64183
thirushaun <thirushaun74@yahoo.com>
2025-01-03 23:32:54 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,23 @@
-// Import the functions you need from the Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+// Import the necessary Firebase components
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCfagQ1fcsslhZ33xm1QBqGOYO_JsqEkeg",
-    authDomain: "medivironap.firebaseapp.com",
-    projectId: "medivironap",
-    storageBucket: "medivironap.firebasestorage.app",
-    messagingSenderId: "440212618885",
-    appId: "1:440212618885:web:11e85acba754ea9fd273ec",
-    measurementId: "G-0ZTM2WNX7M"
+  apiKey: "AIzaSyCfagQ1fcsslhZ33xm1QBqGOYO_JsqEkeg",
+  authDomain: "medivironap.firebaseapp.com",
+  projectId: "medivironap",
+  storageBucket: "medivironap.firebasestorage.app",
+  messagingSenderId: "440212618885",
+  appId: "1:440212618885:web:11e85acba754ea9fd273ec",
+  measurementId: "G-0ZTM2WNX7M"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
 const db = getFirestore(app);
+
+// Export the Firestore database
+export { db };
