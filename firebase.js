a70dede5e9e22ae620e998d8648b96ebeba84b11
thirushaun <thirushaun74@yahoo.com>
2025-01-04 00:37:59 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
-// Import Firebase functions
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; 
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,8 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Get Firestore instance
 const db = getFirestore(app);
-
-export { db };
