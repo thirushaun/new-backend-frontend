7f7d6a53f613061c8f1c04c9071f482e476c7451
thirushaun <thirushaun74@yahoo.com>
2025-01-04 17:25:29 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,23 @@
-<!-- Include Firebase CDN -->
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"></script>
+// Import the necessary Firestore functions from the Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-<script>
-  // Your Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCfagQ1fcsslhZ33xm1QBqGOYO_JsqEkeg",
-    authDomain: "medivironap.firebaseapp.com",
-    projectId: "medivironap",
-    storageBucket: "medivironap.appspot.com",
-    messagingSenderId: "440212618885",
-    appId: "1:440212618885:web:11e85acba754ea9fd273ec",
-    measurementId: "G-0ZTM2WNX7M"
-  };
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfagQ1fcsslhZ33xm1QBqGOYO_JsqEkeg",
+  authDomain: "medivironap.firebaseapp.com",
+  projectId: "medivironap",
+  storageBucket: "medivironap.firebasestorage.app",
+  messagingSenderId: "440212618885",
+  appId: "1:440212618885:web:11e85acba754ea9fd273ec",
+  measurementId: "G-0ZTM2WNX7M"
+};
 
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore(app);
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
 
-  // You can now interact with Firestore, for example:
-  function saveAppointmentData(data) {
-    db.collection("appointments").add(data)
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch((error) => {
-        console.error("Error adding document: ", error);
-      });
-  }
-</script>
+// Initialize Firestore database
+const db = getFirestore(app);
+
+// Export Firestore db instance for use in other files
+export { db };
