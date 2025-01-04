f47184a7061a4c0b08e279570f857448d394683e
thirushaun <thirushaun74@yahoo.com>
2025-01-05 00:52:24 +0800

    Update firebase.js
    delete file

--- firebase.js
+++ firebase.js
@@ -1,18 +1,1 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCfagQ1fcsslhZ33xm1QBqGOYO_JsqEkeg",
-    authDomain: "medivironap.firebaseapp.com",
-    projectId: "medivironap",
-    storageBucket: "medivironap.firebasestorage.app",
-    messagingSenderId: "440212618885",
-    appId: "1:440212618885:web:11e85acba754ea9fd273ec",
-    measurementId: "G-0ZTM2WNX7M"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
