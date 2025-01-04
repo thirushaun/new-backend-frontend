280e83d4df75a28e3a41b99eaae8077daf8a2f5a
thirushaun <thirushaun74@yahoo.com>
2025-01-04 21:25:28 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,8 @@
-// Firebase Firestore Initialization
+// Import necessary Firebase functions
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCfagQ1fcsslhZ33xm1QBqGOYO_JsqEkeg",
     authDomain: "medivironap.firebaseapp.com",
@@ -9,10 +13,9 @@ const firebaseConfig = {
     measurementId: "G-0ZTM2WNX7M"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-// Initialize Firestore database
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 // Export Firestore db instance for use in other files
 export { db };
